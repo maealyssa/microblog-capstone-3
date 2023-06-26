2ed1f2679d6de92449f6d26391013b03d02f3d37
elviraperez <magnoperezelvira@gmail.com>
2023-06-26 11:10:44 -0700

    created registration.js
    created functionality for full name, username, password, and a submit button.

---
+++ scripts/registration.js
@@ -0,0 +1,28 @@
+'use strict';
+
+//calling to the base API
+const apiBaseURL = "https://microbloglite.herokuapp.com";
+
+//new user request
+function newUser() {
+    const un = document.getElementById('UserName').value;
+    const fn = document.getElementById('fullName').value;
+    const ps = document.getElementById('password').value;
+
+    const requestBody = {
+        username: un,
+        fullName: fn,
+        password: ps
+    };
+
+    //fetching from api
+    fetch(apiBaseURL + '/api/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody)
+    })
+        .then(response => response.json())
+        .then(window.location.href='index.html')
+};
